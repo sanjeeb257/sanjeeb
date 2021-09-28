ffb020dd07d66f34de037a93ce5b32a04d3062e0
sanjeeb257 <srisanjeebpati@gmail.com>
2021-09-28 22:23:41 +0530

    new one

---
+++ ConcastSeleniumfrm/src/test/java/Com/concast/contactTest/ContactTest.java
@@ -0,0 +1,28 @@
+package Com.concast.contactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test(groups="Smoke Test")
+	public void createcontactTest()
+	{
+		System.out.println("Test Yantra ");
+	}
+	@Test(groups="Regression Test")
+	public void createcontactWithoutOrgTest()
+	{
+		System.out.println("Test Yantra vvvv ");
+	}
+	@Test(groups="Smoke Test")
+	public void deletecontactTest()
+	{
+		System.out.println("Test Yantra ");
+	}
+	@Test(groups="Smoke Test")
+	public void createcontactTest1()
+	{
+		System.out.println("Test Yantra ");
+	}
+
+}
